468f01eb660b45316e0296b2d95fc4d467117c74
GreMaxonForPda <maksgrek287@gmail.com>
2023-02-06 19:44:53 +0300

    initial commit

---
+++ mysite/bboard/views.py
@@ -0,0 +1,13 @@
+# from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from .models import Bb
+
+def index(request):
+    s = "Тут будут объявления\r\n\r\n\r\n"
+    for bb in Bb.objects.order_by("-published"):
+        s += bb.title + "\r\n" + bb.content + "\r\n\r\n"
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+
